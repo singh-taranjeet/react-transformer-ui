16fa20e3da751e0c3b63aca1c61be69284de72b2
singh-taranjeet <93113908+singh-taranjeet@users.noreply.github.com>
2023-05-21 14:07:51 +0530

    Update index.tsx

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -10,24 +10,27 @@ const list = [
             <p>Create a pattern string in <i>prefix</i> <i>seperator</i> <i>element type</i> <i>seperator</i> <i>stringified json or string</i> <i>seperator</i> <i>suffix</i> format.</p>
             <p>The default values are as follows:</p>
             <ul>
-                <li class="px-2 py-1">
-                    <b class="font-semibold">Prefix</b>: {'<<'}
+                <li class="px-2 py-1 flex">
+                    <b class="font-semibold">Prefix</b>: {<div class="rounded bg-slate-200 px-2 w-fit">{'<<'}</div>} <i> To start a pattern</i>
                 </li>
-                <li class="px-2 py-1">
-                    <b class="font-semibold">Seperator</b>: {'|'}
+                <li class="px-2 py-1 flex">
+                    <b class="font-semibold">Seperator</b>: <div class="rounded bg-slate-200 px-2 w-fit">{'|'}</div> <i> To divide different components of a pattern</i>
                 </li>
-                <li class="px-2 py-1">
-                    <b class="font-semibold">Suffix</b>: {'>>'}
+                <li class="px-2 py-1 flex">
+                    <b class="font-semibold">Suffix</b>: <div class="rounded bg-slate-200 px-2 w-fit">{'>>'}</div> <i> To end a pattern</i>
                 </li>
-                <li class="px-2 py-1">
+                <li class="px-2 py-1 flex">
                     <b class="font-semibold">Element type</b>: HTML element tag or custom component
                 </li>
-                <li class="px-2 py-1">
+                <li class="px-2 py-1 flex">
                     <b class="font-semibold">Data</b>: Stringified json string or a string
                 </li>
             </ul>
 
-            <div class="flex gap-2">
+            <div class='mt-2 pb-2'>
+            <p class='font-bold pb-2'>Following is an example how strings can get converted to:</p>
+            <div class="flex gap-2 flex-col md:flex-row ">
+            
                 <code class="rounded bg-slate-200 px-2">
                     {'<Replacer> This is very<<|Bold|powerfull>>.</Replacer>'}
                 </code>
@@ -37,6 +40,7 @@ const list = [
                 <div class="rounded bg-slate-200 px-2">
                     This is very <b>powerfull</b>.
                 </div>
+            </div>
             </div>
 
         </div>
@@ -53,7 +57,7 @@ export const Home = () => {
         >
             <div class="pt-16 w-full container flex flex-col">
                 <h1 class='text-white text-center  text-4xl pb-6 font-medium'>React Transformer</h1>
-                <p class='text-white text-center pb-4 font-light'>{'Convert simple string patterns to React components with React Transformer'}</p>
+                <p class='text-white text-center pb-4 font-light px-2'>{'Convert simple string patterns to React components with React Transformer'}</p>
                 <a href='https://www.npmjs.com/package/@react-transformer/replacer' target='_blank' class='border rounded px-4 py-2 text-white md:w-fit font-medium mx-auto'>{'Get started'}</a>
             </div>
         </div>
